90c0478f63c1984135ffcef7ecae127d86c14ae3
Ashish Kumar Jha <akjha992@gmail.com>
2021-07-28 19:15:31 +0530

    Update code.js

--- code.js
+++ code.js
@@ -50,7 +50,7 @@ const addButtons = ()=>{
     }
     const superSkipButton = document.querySelector("#superSkipButton");
     if(!superSkipButton){
-        const newButton = '<button style="height:50px;width:200px;text-align:center;position:relative;bottom:-670px;right:-20px;" id="superSkipButton" type="button">Fast Skip</button>'
+        const newButton = '<button style="height:50px;width: 100%;text-align:center;position: ABSOLUTE;bottom:-670px;" id="superSkipButton" type="button">Fast Skip</button>'
         document.querySelector(selectors.buttonContainer).innerHTML+=newButton;
         document.querySelector("#superSkipButton").addEventListener("click", skipAndStart);
     }
